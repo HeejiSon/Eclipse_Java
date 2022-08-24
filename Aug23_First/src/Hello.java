f79e2d6c5fcd14a85fd5b4b9d61b72c9418dddcb
HeejiSon <thsgmlwl@gmail.com>
2022-08-24 16:22:11 +0900

    학원 1일차 - 주석 , 단축키

---
+++ Aug23_First/src/Hello.java
@@ -0,0 +1,32 @@
+// 주석(Comment) : 필기, 기능설명
+
+/*
+ * 여러줄 주석
+ */
+
+/**
+ * 내가 작업한 거 다른 사람에게 설명서 만들어 줄 때 사용하는 거
+ * 
+ * @author pc
+ *
+ */
+// 한 줄 주석 넣기/풀기 	: ( 지우고싶은 줄 아무곳에서) ctrl + /
+// 자동정렬			: ctrl + shift + f
+// 자동 완성			: ctrl + space
+// 한줄 삭제			: ctrl + d
+// 실행 				: ctrl + f11
+// 해당 줄 이동		: alt + 화살표 위아래 방향키
+// 해당 줄 복사 		: ctrl + alt + 화살표 위 아래 방향키
+
+
+public class Hello {
+	// main method 자동완성 : main이라고 쓰고 + ctrl + space
+	public static void main(String[] args) {
+		System.out.println("방찬우는 제꺼입니다~");
+		// syso쓰고 자동완성(ctrl + space)
+		System.out.println("♥♥♥♥♥♥");
+		
+		
+	}
+
+}
