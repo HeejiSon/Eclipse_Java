1840099fd4b4044d638b7830db502295e9f1bd83
HeejiSon <thsgmlwl@gmail.com>
2022-08-24 16:36:34 +0900

    학원 2일차 cmd에서 자바 실행

---
+++ Aug24_0_LearnJava/src/Hello.java
@@ -0,0 +1,17 @@
+// ctrl + m : 창 크기조절
+
+// Encoding
+// 		사람이 입력한 글자를 컴퓨터가 사용할 수 있도록 
+//		MS949, UTF-8, EUC-KR, ...
+
+// javac.exe : JDK에 포함되어 있는 주요한 Java 컴파일러(Compiler)
+// Hello.java파일을 ByteCode(바이트코드, 실행을 위한 이진 표현법)로 변환시키는 것 -> Compile
+// 결론
+//		javac.exe: 영어, 한국어, ... -번역 -> 기계어
+//		java.exe: 기계어 -실행 ->
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
